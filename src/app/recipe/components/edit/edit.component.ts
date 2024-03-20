5dcce6d4adf6107ab1c0a73236f710b314c6380b
Akbar-Khaydarkhujayev <akbarboss91@gmail.com>
2024-03-20 10:08:44 +0500

    Edit component bug fix

--- src/app/recipe/components/edit/edit.component.ts
+++ src/app/recipe/components/edit/edit.component.ts
@@ -4,6 +4,8 @@ import { ServiceProductService } from '../../../service-product.service';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IRecipe } from '../../../IRecipe';
+import { IProduct } from '../../../IProduct';
+import { CommonModule } from '@angular/common';
 
 function findIndexByID(jsonArray: any[], indexToFind: number): number {
   return jsonArray.findIndex((item) => item.id === indexToFind);
@@ -12,7 +14,7 @@ function findIndexByID(jsonArray: any[], indexToFind: number): number {
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css',
 })
@@ -32,7 +34,7 @@ export class EditComponent implements OnInit {
     },
   };
 
-  productObject: any;
+  products: IProduct[] = [];
 
   ngOnInit() {
     this.recipeService
@@ -41,7 +43,7 @@ export class EditComponent implements OnInit {
         this.recipeToEdit = result;
       });
     this.productService.getAllProducts().subscribe((result) => {
-      this.productObject = result;
+      this.products = result;
     });
   }
 
@@ -51,9 +53,7 @@ export class EditComponent implements OnInit {
 
   editRecipe() {
     this.recipeToEdit.product =
-      this.productObject[
-        findIndexByID(this.productObject, this.recipeToEdit.productID)
-      ];
+      this.products[findIndexByID(this.products, this.recipeToEdit.productID)];
 
     this.recipeService.editRecipe(this.recipeToEdit).subscribe((res) => {
       alert('Changes has been updated');
